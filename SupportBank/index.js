de01b085e040bd235e930aa0e9b5a06bed0e400a
Jamie Fox <Jamie.Fox@softwire.com>
2018-06-27 15:51:28 +0100

    find accounts for everyone

--- SupportBank/index.js
+++ SupportBank/index.js
@@ -5,11 +5,27 @@ var content = fs.readFileSync(file, "utf8");
 
 Papa.parse(content, { //parse into a "matrix" array where each element is an array of the rows 
     complete: function(results) {
-        console.log("Finished:", results.data);
+        //console.log("Finished:", results.data);
+        transactions = results.data.slice(1,8); //remove global if possible
     }
 });
 
-var accounts = {};
-for (i=0; i<results.data.length; i++) {
 
-}.
+var accounts = {};
+for (i=0; i<transactions.length; i++) {
+    payFrom = transactions[i][1];
+    payTo = transactions[i][2];
+    payAmount = parseFloat(transactions[i][4]);
+    if (typeof accounts[payFrom] !== 'undefined') { //if the person has an account already
+        accounts[payFrom] = accounts[payFrom] - payAmount;
+    } else {
+        accounts[payFrom] = -payAmount;
+    }
+    if (typeof accounts[payTo] !== 'undefined') { //if the person has an account already
+        accounts[payTo] = accounts[payTo] + payAmount;
+    } else {
+        accounts[payTo] = payAmount;
+    }
+}
+
+console.log(accounts)
