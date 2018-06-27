305e466c814d68bf0e24a1dcdd54d117bb6a7b93
Jamie Fox <Jamie.Fox@softwire.com>
2018-06-27 15:21:01 +0100

    parse CSV

--- SupportBank/index.js
+++ SupportBank/index.js
@@ -5,9 +5,11 @@ var content = fs.readFileSync(file, "utf8");
 
 Papa.parse(content, { //parse into a "matrix" array where each element is an array of the rows 
     complete: function(results) {
-        console.log("Finished:", results.data, results.data.length);
+        console.log("Finished:", results.data);
     }
 });
 
-/*var dict = {};
-for (i=0:i<)*/
+var accounts = {};
+for (i=0; i<results.data.length; i++) {
+
+}
